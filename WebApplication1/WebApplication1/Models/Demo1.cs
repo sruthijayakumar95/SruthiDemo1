83acbc2faabcd1fe2633b24ad357e107bcb3d263
sruthijayakumar95 <34881097+sruthijayakumar95@users.noreply.github.com>
2017-12-27 14:53:06 +0530

    Enter pincode

--- WebApplication1/WebApplication1/Models/Demo1.cs
+++ WebApplication1/WebApplication1/Models/Demo1.cs
@@ -10,5 +10,6 @@ namespace WebApplication1.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string City { get; set; }
+        public int Pincode { get; set; }
     }
 }
