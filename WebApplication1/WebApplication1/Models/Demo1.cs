b92ddb74444ef14b80f8c541bf09d46da6c76bea
sruthijayakumar95 <34881097+sruthijayakumar95@users.noreply.github.com>
2017-12-27 14:47:55 +0530

    EmployeeDemo

---
+++ WebApplication1/WebApplication1/Models/Demo1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Demo1
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+    }
+}
